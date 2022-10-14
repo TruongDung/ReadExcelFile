2950194bec7cb89ba3a9f14372d0909e577a38a1
David Truong <truongdung0502@gmail.com>
2022-10-14 14:24:05 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
 using ExcelApp = Microsoft.Office.Interop.Excel;
 
 ExcelApp.Application excelApp = new ExcelApp.Application();
@@ -11,20 +9,17 @@ ExcelApp.Range excelRange = excelSheet.UsedRange;
 int rows = excelRange.Rows.Count;
 int cols = excelRange.Columns.Count;
 
-var list1 = new List<string>();
+var stringList = new List<string>();
 for (int i = 1; i <= rows; i++)
 {
-    //create new line
     Console.Write("\r\n");
     for (int j = 2; j <= 2; j++)
     {
-        //write the console
         if (excelRange.Cells[i, j] != null && excelRange.Cells[i, j].Value2 != null)
-            list1.Add( "'" + excelRange.Cells[i, j].Value2.ToString() + "'");
-            //Console.Write(excelRange.Cells[i, j].Value2.ToString());
+            stringList.Add( "'" + excelRange.Cells[i, j].Value2.ToString() + "'");
     }
 }
-var a2 = string.Join(",", list1);
+var result = string.Join(",", stringList);
 
 excelApp.Quit();
 System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
